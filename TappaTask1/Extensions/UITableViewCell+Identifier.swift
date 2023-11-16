2eb952e4470e9b8aa11b44ddbd74e21bd9450684
Ilya Trukhau <ilya.trukhau@solbeg.com>
2023-11-16 12:08:43 +0100

    Create extension of UITableViewCell to get identifier

---
+++ TappaTask1/Extensions/UITableViewCell+Identifier.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+Identifier.swift
+//  TappaTask1
+//
+//  Created by Ilya Trukhau on 11/16/23.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
